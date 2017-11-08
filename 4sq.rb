d6cb98505cbf58e70444d5085d8aabc1ce10f495
okitan <okitakunio@gmail.com>
2017-11-08 23:17:10 +0900

    :bulb: implement many things

--- 4sq.rb
+++ 4sq.rb
@@ -4,16 +4,83 @@ require "thor"
 
 require "foursquare2"
 
+require_relative "zipcode"
+
 require "pry"
+
+module Foursquare2
+  module Venues
+    # undocumented
+    def children(venue_id, options = {})
+      response = connection.get do |req|
+        req.url "venues/#{venue_id}/children", options
+      end
+      return_error_or_body(response, response.body.response.children)
+    end
+  end
+end
 
 # suppress warning
 Hashie.logger = Logger.new("/dev/null")
 
 class Foursquare < Thor
+  class << self
+    def display_options
+      option :fields, type: :array, default: %w[ name url zip state city address crossStreet phone stats categories ]
+      option :format, aliases: "-f", default: "table", enum: %w[ table ltsv csv ]
+    end
+  end
+
   desc "show *VENUE_ID", "show venues"
+  display_options
   def show(*venues)
-    venues.each do |id|
-      p client.venue(id)
+    format(venues.map {|id| compact_venue(client.venue(id), options[:fields]) }, options[:fields], options[:format])
+  end
+
+  desc "fix *VENUE_ID", "show venues"
+  option :parentId, desc: "parent venue id (addresses will be copied from here)"
+  option :state
+  option :city
+  option :address
+  option :crossStreet
+
+  option :auto,    type: :boolean, default: false
+  option :dry_run, type: :boolean, default: true
+  option :format, aliases: "-f", default: "table", enum: %w[ table ltsv csv ]
+  def fix(*venues)
+    venues = venues.map {|id| client.venue(id) }
+
+    patches = if options[:parentId]
+      parent_venue = client.venue(options[:parentId])
+
+      venues.map do |venue|
+        %i[ zip state city address crossStreet ].each.with_object("parentId" => options[:parentId]) do |key, hash|
+          hash[key.to_s] = get_readable_value(parent_venue, key) unless (get_readable_value(venue, key) || "") == get_readable_value(parent_venue, key)
+        end
+      end
+    elsif options[:auto]
+      venues.map {|venue| diff(venue, options[:fields]) }
+    end
+
+    %i[ state city address crossStreet ].each do |key|
+      if options[key]
+        patches.each {|patch| patch[key.to_s] = options[key] }
+      end
+    end
+
+    venues.zip(patches).each do |venue, patch|
+      if options[:dry_run]
+        puts get_readable_value(venue, "name")
+        show_changes(venue, patch)
+      else
+        unless patch.empty?
+          # TODO: emit fields to be updated
+          client.propose_venue_edit(venue["id"], patch)
+
+          fields = (%w[ name url ] + patch.keys).uniq
+          format([ compact_venue(client.venue(venue["id"]), fields) ], fields, options[:format])
+        end
+      end
     end
   end
 
@@ -24,18 +91,38 @@ class Foursquare < Thor
 
   option :limit, desc: "number of results (max 50)",  default: 10
 
-  option :fields, typa: :array, default: %w[ name url categories ]
-  option :format, aliases: "-f", default: "table", enum: %w[ table ltsv csv ]
+  display_options
   def search(word)
     venues = client.search_venues(
       query:  word,
       near:   options[:near],
       intent: options[:intent],
       limit:  options[:limit],
-      radius: 100_000,
+      radius: 10_000,
     )["venues"]
 
-    format(venues, options[:fields], options[:format])
+    format(venues.map {|venue| compact_venue(venue, options[:fields]) }, options[:fields], options[:format])
+  end
+
+  desc "subvenues VENUE_ID", "get sub venues of venue"
+  option :show_near_venues, type: :boolean, default: false
+
+  option :limit, desc: "number of results (max 50)",  default: 50
+  display_options
+  def subvenues(id)
+    parent_venue = client.venue(id)
+
+    grouped_venues = client.children(id)
+    venues = grouped_venues.groups.map {|g| g.items }.flatten.sort {|a, b| a.name <=> b.name }
+
+    format(venues.map {|venue| compact_venue(venue, options[:fields]) }, options[:fields], options[:format])
+  end
+
+  desc "close *VENUE_ID", "close venues"
+  def close(*venues)
+    venues.each do |id|
+      p client.flag_venue(id, problem: "closed")
+    end
   end
 
   # https://developer.foursquare.com/docs/api/venues/flag
@@ -50,24 +137,19 @@ class Foursquare < Thor
 
   protected
   def client
-    @client ||= Foursquare2::Client.new(oauth_token: ENV["ACCESS_TOKEN"], api_version: "20170801")
+    @client ||= Foursquare2::Client.new(
+      oauth_token: ENV["ACCESS_TOKEN"], api_version: "20170801",
+      #connection_middleware: [ FaradayMiddleware::FollowRedirects ]
+    )
   end
 
-  def format(venues, fields, format, first: false)
-    venues = [*venues]
-
+  def format(venues, fields, format)
     return if venues.empty?
-
-    venues = venues.map do |venue|
-      fields.each.with_object({}) do |field, hash|
-        hash[field] = get_readable_value(venue, field)
-      end
-    end
 
     case format
     when "table"
       require "hirb-unicode"
-      puts Hirb::Helpers::AutoTable.render(venues, resize: false)
+      puts Hirb::Helpers::AutoTable.render(venues, resize: false, fields: fields)
     when "ltsv"
       require "ltsv"
       venues.each {|v| puts LTSV.dump(v) }
@@ -78,15 +160,110 @@ class Foursquare < Thor
     end
   end
 
+  def show_changes(venue, patch)
+    require "hirb-unicode"
+    changes = patch.map do |key, value|
+      { "key" => key, "before" => get_readable_value(venue, key), "after" => value }
+    end
+
+    puts Hirb::Helpers::AutoTable.render(changes, resize: false, fields: %w[ key before after ])
+  end
+
+  def compact_venue(venue, fields)
+    fields.each.with_object({}) do |field, hash|
+      hash[field] = get_readable_value(venue, field)
+    end
+  end
+
   def get_readable_value(venue, field)
-    case field
+    case field.to_s
     when "url"
-      "https://ja.foursquare.com/v/#{venue["id"]}"
+      venue["shortUrl"] ||"https://ja.foursquare.com/v/#{venue["id"]}"
+    when "zip"
+      venue["location"]["postalCode"]&.sub("-", "")
+    when "state"
+      venue["location"]["state"]
+    when "city"
+      city = venue["location"]["city"]
+
+      # 東京, 東京都
+      if city == "東京"
+        address, city = *venue["location"]["formattedAddress"]
+        city&.split(",").first
+      else
+        city
+      end
+    when "address"
+      venue["location"]["address"]
+    when "crossStreet"
+      venue["location"]["crossStreet"]
+    when "fullAddress"
+      address, city = *venue["location"]["formattedAddress"]
+      # address includes m17ned (crossStreet)
+      [ city&.split(",")&.reverse&.join(""), address ].compact.join("")
+    when "phone"
+      venue["contact"]["phone"]
+    when "stats"
+      "#{venue["stats"]["checkinsCount"]}/#{venue["stats"]["usersCount"]}"
     when "categories"
       venue["categories"].map {|c| c["name"] }
     else
       venue[field]
     end
+  end
+
+  def diff(venue, fields)
+    ret = {}
+
+    # zip / state / city / address / crossStreet
+    address = %i[ state city address crossStreet fullAddress ].each.with_object({}) do |k, h|
+      h[k] = get_readable_value(venue, k)
+    end
+
+    begin
+      zip = Zipcode.search_town(address[:state], address[:city], address[:address])
+      ret["zip"] = zip unless zip == get_readable_value(venue, "zip")
+
+      new_address = adjust_address_4sq(address)
+      %i[ state city address crossStreet ].each do |key|
+        ret[key.to_s] = new_address[key] unless (address[key] || "") == new_address[key]
+      end
+    rescue => e
+      warn e
+
+      begin
+        zip = Zipcode.search(address[:fullAddress])
+        ret["zip"] = zip unless zip == get_readable_value(venue, "zip")
+
+        _state, _city, _address = *Zipcode.split_address(address[:fullAddress])
+        new_address = adjust_address_4sq(state: _state, city: _city, address: _address, crossStreet: address[:crossStreet])
+        %i[ state city address crossStreet ].each do |key|
+          ret[key.to_s] = new_address[key] unless (address[key] || "") == new_address[key]
+        end
+      rescue => e
+        warn e
+      end
+    end
+
+    ret
+  end
+
+  def adjust_address_4sq(state:, city:, address:, crossStreet: nil, **_)
+    crossStreet ||= ""
+
+    address = address.tr("０-９", "0-9").gsub(/ー|丁目/, "-")
+
+    i = address.rindex(/\d/)
+    if i
+      address, crossStreet = address[0..i], ((address[i+1..-1] || "") + crossStreet)
+    end
+
+    if state != "東京都" && city.end_with?("区")
+      i = city.index("市")
+      city, address = city[0..i], ((city[i+1..-1] || "") + address)
+    end
+
+    { state: state, city: city, address: address, crossStreet: crossStreet }
   end
 end
 
---
+++ zipcode.rb
@@ -0,0 +1,92 @@
+#!/usr/bin/env ruby
+
+require "yaml"
+
+module Zipcode
+  def split_address(address)
+    regexp = /(...??[都道府県])((?:旭川|伊達|石狩|盛岡|奥州|田村|南相馬|那須塩原|東村山|武蔵村山|羽村|十日町|上越|富山|野々市|大町|蒲郡|四日市|姫路|大和郡山|廿日市|下松|岩国|田川|大村)市|.+?郡(?:玉村|大町|.+?)[町村]|.+?市.+?区|.+?[市区町村])(.+)/
+
+    address.match(regexp).captures
+  end
+
+  def search(address)
+    begin
+      ken, shi, tyou = split_address(address)
+    rescue
+      raise "no match #{address}"
+    end
+
+    search_town(ken, shi, tyou)
+  end
+
+  def search_town(ken, shi, tyou, banti = nil)
+    if dictionary[ken]
+      if dictionary[ken][shi]
+        if dictionary[ken][shi][tyou]
+          group_zipcode(dictionary[ken][shi][tyou])
+        else
+          tyou = tyou.tr("０-９", "0-9")
+          i = tyou.index(/\d/)
+
+          unless i
+            towns = dictionary[ken][shi].keys.select {|town| town.start_with?(tyou) }
+            if towns.size == 1
+              return search_town(ken, shi, towns.first)
+            else
+              if towns.size > 2
+                raise "#{towns.size} candidates found: #{ken} #{shi} #{tyou}"
+              else
+                raise "unknown town: #{ken} #{shi} #{tyou}?"
+              end
+            end
+          end
+
+          tyou, banti = tyou[0..i-1], tyou[i..-1]
+
+          if dictionary[ken][shi][tyou]
+            if dictionary[ken][shi][tyou][banti]
+              dictionary[ken][shi][tyou][banti]
+            elsif dictionary[ken][shi][tyou]["その他"]
+              dictionary[ken][shi][tyou]["その他"]
+            else
+              group_zipcode(dictionary[ken][shi][tyou])
+            end
+          else
+            raise "unknown town: #{ken} #{shi} #{tyou}? #{banti}"
+          end
+        end
+      else
+        raise "unknown city: #{ken} #{shi}? #{tyou}"
+      end
+    else
+      raise "unknown state: #{ken}? #{shi} #{tyou}"
+    end
+  end
+
+  def group_zipcode(hash)
+    return hash unless hash.is_a?(Hash)
+
+    hash.each.with_object(Hash.new { [] }) do |(key, value), h|
+      h[value] << key
+    end
+  end
+
+  def dictionary
+    @dictionary ||= YAML.load_file("KEN_ALL.rev.yaml")
+  end
+
+  module_function *instance_methods
+end
+
+if $0 == __FILE__
+  require "thor"
+
+  class ZipCLI < Thor
+    desc "search", ""
+    def search(address)
+      puts Zipcode.search(address)
+    end
+  end
+
+  ZipCLI.start
+end
